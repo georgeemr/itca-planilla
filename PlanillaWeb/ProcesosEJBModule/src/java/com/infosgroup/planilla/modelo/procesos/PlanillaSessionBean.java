d24dec0e456b5cee5387fa193fefeaf6cfe7028d
g.bsfactory <g.bsfactory@gmail.com>
2011-10-27 20:56:27 +0000

    Actualizacion Entidades - 27-10-2011

--- PlanillaWeb/ProcesosEJBModule/src/java/com/infosgroup/planilla/modelo/procesos/PlanillaSessionBean.java
+++ PlanillaWeb/ProcesosEJBModule/src/java/com/infosgroup/planilla/modelo/procesos/PlanillaSessionBean.java
@@ -15,7 +15,6 @@ import javax.ejb.LocalBean;
 @LocalBean
 public class PlanillaSessionBean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+
     
 }
