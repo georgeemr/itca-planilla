fd9e9169cc36a6598b5ae9b3ee70f257accbe2a9
g.bsfactory <g.bsfactory@gmail.com>
2011-10-27 18:28:34 +0000

    Actualizacion Entidades - 27-10-2011

---
+++ PlanillaWeb/PlanillaWeb-war/src/java/com/infosgroup/planilla/controlador/sessionbean/SessionBeanEMP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.infosgroup.planilla.controlador.sessionbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author root
+ */
+@ManagedBean(name = "SessionBeanEMP")
+@SessionScoped
+public class SessionBeanEMP {
+
+    /** Creates a new instance of SessionBeanEMP */
+    public SessionBeanEMP() {
+    }
+    
+}
