9177bb1cb5ff33fe4c9ba098236962635c4d4e9d
g.bsfactory@gmail.com <g.bsfactory@gmail.com@50c6a9fc-67a5-2ce8-c272-409a90f92b53>
2011-10-31 20:58:13 +0000

    Reclutamiento - Preseleccion Aspirante - 31-10-2011

---
+++ trunk/PlanillaWeb/PlanillaWeb-war/src/java/com/infosgroup/planilla/controlador/modulos/reclutamiento/PreseleccionAspiranteBackendBean.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.infosgroup.planilla.controlador.modulos.reclutamiento;
+
+import com.infosgroup.planilla.modelo.entidades.Concurso;
+import com.infosgroup.planilla.modelo.procesos.ReclutamientoSessionBean;
+import com.infosgroup.planilla.view.JSFUtil;
+import com.infosgroup.planilla.view.TipoMensaje;
+import java.util.Date;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.util.List;
+
+/**
+ *
+ * @author root
+ */
+@ManagedBean(name = "reclutamiento$preseleccionAspirante")
+@ViewScoped
+public class PreseleccionAspiranteBackendBean extends JSFUtil {
+
+    @EJB
+    private ReclutamientoSessionBean reclutamientoSessionBean;
+    private Date fechaInicial;
+    private Date fechaFinal;
+    private List<Concurso> listaConcurso;
+
+    public PreseleccionAspiranteBackendBean() {
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public Date getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(Date fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public List<Concurso> getListaConcurso() {
+        return listaConcurso;
+    }
+
+    public void setListaConcurso(List<Concurso> listaConcurso) {
+        this.listaConcurso = listaConcurso;
+    }
+
+    public String buscarConcurso$action() {
+        
+        if (fechaInicial != null && fechaFinal != null) {
+            if (validaFechas(fechaInicial, fechaFinal) == true) {
+                setListaConcurso(reclutamientoSessionBean.getListaConcursos(fechaInicial, fechaFinal));
+            } else {
+                addMessage("Buscar concurso", "Los rangos de fecha Ingresados no son consistentes.", TipoMensaje.ERROR);
+            }
+        } else {
+            setListaConcurso(reclutamientoSessionBean.getAllConcursos());
+            addMessage("Buscar concurso", "Mostrando todos los concursos", TipoMensaje.INFORMACION);
+        }
+        return null;
+    }
+
+    @Override
+    protected void limpiarCampos() {
+        setFechaInicial(null);
+        setFechaFinal(null);
+    }
+}
