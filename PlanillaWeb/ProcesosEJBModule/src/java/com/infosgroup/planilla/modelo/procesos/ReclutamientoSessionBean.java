738e1f380c4cd7707b6cb0ad69eb07d124edb86f
g.bsfactory <g.bsfactory@gmail.com>
2011-12-06 16:15:57 +0000

    Mapeo de Entidades - 6-12-2011

--- PlanillaWeb/ProcesosEJBModule/src/java/com/infosgroup/planilla/modelo/procesos/ReclutamientoSessionBean.java
+++ PlanillaWeb/ProcesosEJBModule/src/java/com/infosgroup/planilla/modelo/procesos/ReclutamientoSessionBean.java
@@ -6,7 +6,17 @@ package com.infosgroup.planilla.modelo.procesos;
 
 import com.infosgroup.planilla.modelo.entidades.Candidato;
 import com.infosgroup.planilla.modelo.entidades.Concurso;
+import com.infosgroup.planilla.modelo.entidades.EstadoConcurso;
+import com.infosgroup.planilla.modelo.entidades.EstadoConcursoPK;
+import com.infosgroup.planilla.modelo.entidades.Puesto;
+import com.infosgroup.planilla.modelo.entidades.PuestoPK;
+import com.infosgroup.planilla.modelo.entidades.TipoPuesto;
+import com.infosgroup.planilla.modelo.entidades.TipoPuestoPK;
+import com.infosgroup.planilla.modelo.facades.CandidatoFacade;
 import com.infosgroup.planilla.modelo.facades.ConcursoFacade;
+import com.infosgroup.planilla.modelo.facades.EstadoConcursoFacade;
+import com.infosgroup.planilla.modelo.facades.PuestoFacade;
+import com.infosgroup.planilla.modelo.facades.TipoPuestoFacade;
 import java.util.ArrayList;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -21,19 +31,70 @@ import java.util.List;
 @Stateless
 @LocalBean
 public class ReclutamientoSessionBean {
-
+    
+    @EJB
+    private EstadoConcursoFacade estadoConcursoFacade;
+    @EJB
+    private TipoPuestoFacade tipoPuestoFacade;
+    @EJB
+    private PuestoFacade puestoFacade;
+    @EJB
+    private CandidatoFacade candidatoFacade;
     @EJB
     private ConcursoFacade concursoFacade;
-
+    
     public List<Concurso> getListaConcursos(Date fechaInicial, Date fechaFinal) {
         return concursoFacade.getConcursosByDate(fechaInicial, fechaFinal);
     }
-
-    public List<Concurso> getAllConcursos() {
-        return concursoFacade.findAll();
+    
+    public List<Concurso> getConcursosEvaluados(Integer empresa) {
+        return concursoFacade.getConcursosActivos(empresa);
     }
-
+    
+    public List<EstadoConcurso> getEstadoConcursosByEmpresa(Integer empresa) {
+        return estadoConcursoFacade.findEstadoConcursoByEmpresa(empresa);
+    }
+    
+    public List<Puesto> getPuestosByEmpresa(Integer empresa) {
+        return puestoFacade.findPuestoByEmpresa(empresa);
+    }
+    
+    public List<TipoPuesto> getTipoPuestosByEmpresa(Integer empresa) {
+        return tipoPuestoFacade.findTipoPuestoByEmpresa(empresa);
+    }
+    
+    public Integer getMaxConcurso(Integer empresa) {
+        return concursoFacade.getMax(empresa).intValue();
+    }
+    
+    public Puesto findPuestoById(PuestoPK pkPuesto) {
+        return puestoFacade.find(pkPuesto);
+    }
+    
+    public EstadoConcurso findEstadoConcursoById(EstadoConcursoPK estadoConcursoPK) {
+        return estadoConcursoFacade.find(estadoConcursoPK);
+    }
+    
+    public TipoPuesto findTipoPuestoById(TipoPuestoPK tipoPuestoPK) {
+        return tipoPuestoFacade.find(tipoPuestoPK);
+    }
+    
+    public void guardarConcurso(Concurso c) {
+        concursoFacade.create(c);
+    }
+    
     public List<Candidato> getCandidatosByConcurso(Concurso c) {
-        return (c != null) ? concursoFacade.find(c.getConcursoPK()).getCandidatoList() :  new ArrayList<Candidato>();
+        return (c != null) ? concursoFacade.find(c.getConcursoPK()).getCandidatoList() : new ArrayList<Candidato>();
+    }
+    
+    public void CambioEstadoCandidato(Concurso concurso, List<Candidato> listaCandidatos, String estado) {
+        for (Candidato c : concurso.getCandidatoList()) {
+            c.setEstado("A");
+            candidatoFacade.edit(c);
+        }
+        for (Candidato c : listaCandidatos) {
+            c.setEstado(estado);
+            candidatoFacade.edit(c);
+        }
     }
 }
