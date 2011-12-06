ef8010335e253f10c728eca27a162ea0d191f4e3
g.bsfactory@gmail.com <g.bsfactory@gmail.com@50c6a9fc-67a5-2ce8-c272-409a90f92b53>
2011-12-06 16:13:13 +0000

    Mapeo de Entidades - 6-12-2011

---
+++ trunk/PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/facades/EstadoConcursoFacade.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.infosgroup.planilla.modelo.facades;
+
+import com.infosgroup.planilla.modelo.entidades.EstadoConcurso;
+import com.infosgroup.planilla.modelo.entidades.EstadoConcursoPK;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author root
+ */
+@Stateless
+public class EstadoConcursoFacade extends AbstractFacade<EstadoConcurso, EstadoConcursoPK> {
+
+    @PersistenceContext(unitName = "PlanillaWeb-ejbPU")
+    private EntityManager em;
+
+    public EstadoConcursoFacade() {
+        super(EstadoConcurso.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public List<EstadoConcurso> findEstadoConcursoByEmpresa(Integer empresa) {
+        List<EstadoConcurso> puestos = new ArrayList<EstadoConcurso>();
+        puestos.addAll(getEntityManager().createNamedQuery("EstadoConcurso.findByCodCia", EstadoConcurso.class).setParameter("codCia", empresa).getResultList());
+        return puestos;
+    }
+}
