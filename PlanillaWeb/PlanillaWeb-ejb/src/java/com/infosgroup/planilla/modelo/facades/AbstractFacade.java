49e2bfbe6cacce9267f796b078a88b7389684df6
g.bsfactory <g.bsfactory@gmail.com>
2011-12-06 16:13:13 +0000

    Mapeo de Entidades - 6-12-2011

--- PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/facades/AbstractFacade.java
+++ PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/facades/AbstractFacade.java
@@ -1,7 +1,7 @@
 /*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.infosgroup.planilla.modelo.facades;
 
 import java.util.List;
@@ -15,62 +15,28 @@ import javax.persistence.EntityManager;
 public abstract class AbstractFacade<T, P> {
 
     private Class<T> entityClass;
-=======
-*
-* @author root
-*/
-public abstract class AbstractFacade<T, P>
-{
-private Class<T> entityClass;
->>>>>>> .r199
 
-public AbstractFacade(Class<T> entityClass)
-{
-this.entityClass = entityClass;
-}
+    public AbstractFacade(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
 
-protected abstract EntityManager getEntityManager();
+    protected abstract EntityManager getEntityManager();
 
-<<<<<<< .mine
     public void create(T entity) {
         getEntityManager().persist(entity);
         getEntityManager().flush();
     }
-=======
-public void create(T entity)
-{
-getEntityManager().persist(entity);
-getEntityManager().flush();
-}
->>>>>>> .r199
 
-<<<<<<< .mine
     public void edit(T entity) {
         getEntityManager().merge(entity);
         getEntityManager().flush();
     }
-=======
-public void edit(T entity)
-{
-getEntityManager().merge(entity);
-getEntityManager().flush();
-}
->>>>>>> .r199
 
-<<<<<<< .mine
     public void remove(T entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
         getEntityManager().flush();
     }
-=======
-public void remove(T entity)
-{
-getEntityManager().remove(getEntityManager().merge(entity));
-getEntityManager().flush();
-}
->>>>>>> .r199
 
-<<<<<<< .mine
     public void refresh(T entity) {
         getEntityManager().refresh(entity);
     }
@@ -78,31 +44,22 @@ getEntityManager().flush();
     public T find(P id) {
         return getEntityManager().find(entityClass, id);
     }
-=======
-public T find(P id)
-{
-return getEntityManager().find(entityClass, id);
-}
->>>>>>> .r199
 
-public List<T> findAll()
-{
-javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-cq.select(cq.from(entityClass));
-return getEntityManager().createQuery(cq).getResultList();
-}
+    public List<T> findAll() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
 
-public List<T> findRange(int[] range)
-{
-javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-cq.select(cq.from(entityClass));
-javax.persistence.Query q = getEntityManager().createQuery(cq);
-q.setMaxResults(range[1] - range[0]);
-q.setFirstResult(range[0]);
-return q.getResultList();
-}
+    public List<T> findRange(int[] range) {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        javax.persistence.Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0]);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
 
-<<<<<<< .mine
     public int count() {
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
@@ -110,19 +67,4 @@ return q.getResultList();
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         return ((Long) q.getSingleResult()).intValue();
     }
-=======
-public int count()
-{
-javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
-cq.select(getEntityManager().getCriteriaBuilder().count(rt));
-javax.persistence.Query q = getEntityManager().createQuery(cq);
-return ((Long) q.getSingleResult()).intValue();
->>>>>>> .r199
-}
-
-public void refresh(T entity)
-{
-getEntityManager().refresh(entity);
-}
 }
