5747a8c556b6bad09a19d7626fa27c23c8aa2574
g.bsfactory <g.bsfactory@gmail.com>
2011-12-07 00:10:22 +0000

    Mapeo de Entidades - 6-12-2011

---
+++ PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/estructuras/OperadorCriterio.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.infosgroup.planilla.modelo.estructuras;
+
+/**
+ *
+ * @author root
+ */
+public enum OperadorCriterio {
+    between, equal, lessThan, moreTran;
+}
