47e255d24d343b7d390ab5319ae897536fe2c0aa
g.bsfactory@gmail.com <g.bsfactory@gmail.com@50c6a9fc-67a5-2ce8-c272-409a90f92b53>
2011-12-08 21:29:35 +0000

    Mapeo de Entidades - 6-12-2011

--- trunk/PlanillaWeb/PlanillaWeb-war/src/java/com/infosgroup/planilla/controlador/sessionbean/SessionBeanREC.java
+++ trunk/PlanillaWeb/PlanillaWeb-war/src/java/com/infosgroup/planilla/controlador/sessionbean/SessionBeanREC.java
@@ -4,9 +4,13 @@
  */
 package com.infosgroup.planilla.controlador.sessionbean;
 
+import com.infosgroup.planilla.modelo.entidades.Candidato;
 import com.infosgroup.planilla.modelo.entidades.Concurso;
+import com.infosgroup.planilla.modelo.entidades.CriteriosXPuesto;
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.primefaces.model.DualListModel;
 
 /**
  *
@@ -14,15 +18,18 @@ import javax.faces.bean.SessionScoped;
  * <pre>Bean de Session utilizado para almacenar los datos 
  * de Reclutamiento y preseleccion de candidatos.</pre>
  */
-@ManagedBean(name="SessionBeanREC")
+@ManagedBean(name = "SessionBeanREC")
 @SessionScoped
-public class SessionBeanREC {
+public class SessionBeanREC implements Serializable {
 
+    /**
+     * Comun a todos los flujos de Reclutamiento 
+     */
     private Concurso concursoSeleccionado;
-    
+
     public SessionBeanREC() {
     }
-    
+
     public Concurso getConcursoSeleccionado() {
         return concursoSeleccionado;
     }
