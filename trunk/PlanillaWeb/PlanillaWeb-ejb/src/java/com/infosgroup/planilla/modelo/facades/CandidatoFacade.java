6b01392cd185ea0ad7a50fe31ac68c1a84fe384e
g.bsfactory@gmail.com <g.bsfactory@gmail.com@50c6a9fc-67a5-2ce8-c272-409a90f92b53>
2011-12-12 16:34:35 +0000

    Cambios por base de datos Oracle - 1212-2011

--- trunk/PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/facades/CandidatoFacade.java
+++ trunk/PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/facades/CandidatoFacade.java
@@ -31,10 +31,7 @@ import java.util.logging.Logger;
 @LocalBean
 public class CandidatoFacade extends AbstractFacade<Candidato, CandidatoPK> {
 
-    
-
     private enum operacion { equal, between };
-    
     
     @PersistenceContext(unitName = "PlanillaWeb-ejbPU")
     private EntityManager em;
@@ -54,25 +51,21 @@ public class CandidatoFacade extends AbstractFacade<Candidato, CandidatoPK> {
         return listaCandidatos;
     }
 
+    /**
+     * Devuelve una lista de candidatos, con todos los criterios del puesto del concurso seleccionado.
+     */
     public List<Candidato> getCandidatoConCriteriosPuesto(Concurso c, List<String> criterios) {
         List<Candidato> listaCandidatos = new ArrayList<Candidato>();
-        for ( Candidato candidato:  findByCanditadoByEmpresa( c.getConcursoPK().getCodCia() )  ){
-            if ( validaCriterios( c.getConcursoPK().getCodCia(), c.getPuesto().getPuestoPK().getCodPuesto(), candidato.getCandidatoPK().getCodCandidato(), criterios) == 1 ){
-                listaCandidatos.add( candidato );
+        for (Candidato candidato : findByCanditadoByEmpresa(c.getConcursoPK().getCodCia())) {
+            if (validaCriterios(c.getConcursoPK().getCodCia(), c.getPuesto().getPuestoPK().getCodPuesto(), candidato.getCandidatoPK().getCodCandidato()/*, criterios*/) == 1) {
+                listaCandidatos.add(candidato);
             }
         }
         return listaCandidatos;
     }
 
-    public Integer validaCriterios(Long empresa, Long puesto, Long candidato, List<String> criterios) {
-        //codCia + ":" + puesto + ":" + criterio + ":" + correlativo + ":" + tipoCriterio;
-        /*String cq = "";
-        
-        for (String qt: criterios ){
-            cq = qt.split(":")[0] + qt.split(":")[2] + qt.split(":")[4];
-        }*/
-        
-        String q ="SELECT t.criteriosXPuestoPK.codCia, t.criteriosXPuestoPK.puesto, "
+    public Integer validaCriterios(Long empresa, Long puesto, Long candidato /*, List<String> criterios */) {
+        String q = "SELECT t.criteriosXPuestoPK.codCia, t.criteriosXPuestoPK.puesto, "
                 + " t.criteriosXPuestoPK.tipoCriterio, t.criteriosXPuestoPK.correlativo, "
                 + " t.valor, t.valorInicialRango, t.valorFinalRango,  "
                 + " u.operador, u.clase, v.campo, v.entidad, v.entidadPK, u "
@@ -80,22 +73,33 @@ public class CandidatoFacade extends AbstractFacade<Candidato, CandidatoPK> {
                 + "WHERE t.criteriosXPuestoPK.codCia = :codCia AND t.criteriosXPuestoPK.puesto = :puesto";
         for (Object o : getEntityManager().createQuery(q).setParameter("codCia", empresa).setParameter("puesto", puesto).getResultList()) {
             ModelConsultaCriterio model = new ModelConsultaCriterio((Object[]) o);
-            Object t = getEntityManager().createQuery("SELECT v." + model.getCampo() + " FROM " + model.getEntidad() + " v WHERE v." + model.getEntidadPK().split(":")[0]+ " = " + empresa + " AND v." + model.getEntidadPK().split(":")[1] + " =  " + candidato).setMaxResults(1).getSingleResult();
+            Object t = getEntityManager().createQuery("SELECT v." + model.getCampo() + " FROM " + model.getEntidad() + " v WHERE v." + model.getEntidadPK().split(":")[0] + " = " + empresa + " AND v." + model.getEntidadPK().split(":")[1] + " =  " + candidato).setMaxResults(1).getSingleResult();
             if (model.getOperacion().equals(operacion.equal.toString())) {
-                if (model.getValor() == null){ return 0; }
+                if (model.getValor() == null) {
+                    return 0;
+                }
                 if (model.getClase().equals("java.lang.String")) {
-                    if ( !t.toString().equals( model.getValor().toString() )) return 0;
+                    if (!t.toString().equals(model.getValor().toString())) {
+                        return 0;
+                    }
                 } else if (model.getClase().equals("java.lang.Integer")) {
-                    if ( !t.toString().equals(model.getValor().toString())) return 0;
+                    if (!t.toString().equals(model.getValor().toString())) {
+                        return 0;
+                    }
                 } else if (model.getClase().equals("java.util.Date")) {
-                    if (! new SimpleDateFormat("dd/MM/yyyy").format((Date) t).equals(new SimpleDateFormat("dd/MM/yyyy").format(model.getValor().toString()))) { return 0; }
+                    if (!new SimpleDateFormat("dd/MM/yyyy").format((Date) t).equals(new SimpleDateFormat("dd/MM/yyyy").format(model.getValor().toString()))) {
+                        return 0;
+                    }
                 }
             } else if (model.getOperacion().equals(operacion.between.toString())) {
-                if (model.getValorInicialRango() == null || model.getValorInicialRango() == null){ return 0; }
-                
+                if (model.getValorInicialRango() == null || model.getValorInicialRango() == null) {
+                    return 0;
+                }
+
                 if (model.getClase().equals("java.lang.Integer")) {
-                    if (!( new Integer(t.toString()) >= new Integer(model.getValorInicialRango().toString()) && new Integer(t.toString()) <= new Integer(model.getValorInicialRango().toString()))) 
-                       { return 0; }
+                    if (!(new Integer(t.toString()) >= new Integer(model.getValorInicialRango().toString()) && new Integer(t.toString()) <= new Integer(model.getValorInicialRango().toString()))) {
+                        return 0;
+                    }
                 } else if (model.getClase().equals("java.util.Date")) {
                     SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
                     java.util.Date a = null, b = null, c = null;
@@ -114,4 +118,12 @@ public class CandidatoFacade extends AbstractFacade<Candidato, CandidatoPK> {
         }
         return 1;
     }
+    
+    private List<Candidato> getCandidatosByCriterios(){
+        
+        
+        
+        return null;
+    }
+    
 }
