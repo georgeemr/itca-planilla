5597e6f4770f04b2847f13f50ee9c02866d26ff4
g.bsfactory <g.bsfactory@gmail.com>
2011-12-12 17:01:31 +0000

    Cambios por base de datos Oracle - 1212-2011

--- PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/entidades/CriteriosXPuesto.java
+++ PlanillaWeb/PlanillaWeb-ejb/src/java/com/infosgroup/planilla/modelo/entidades/CriteriosXPuesto.java
@@ -5,7 +5,6 @@
 package com.infosgroup.planilla.modelo.entidades;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -15,7 +14,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -26,8 +24,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "CRITERIOS_X_PUESTO")
 @XmlRootElement
-@NamedQueries(
-    {
+@NamedQueries({
     @NamedQuery(name = "CriteriosXPuesto.findAll", query = "SELECT c FROM CriteriosXPuesto c"),
     @NamedQuery(name = "CriteriosXPuesto.findByCodCia", query = "SELECT c FROM CriteriosXPuesto c WHERE c.criteriosXPuestoPK.codCia = :codCia"),
     @NamedQuery(name = "CriteriosXPuesto.findByPuesto", query = "SELECT c FROM CriteriosXPuesto c WHERE c.criteriosXPuestoPK.puesto = :puesto"),
@@ -36,188 +33,116 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "CriteriosXPuesto.findByCorrelativo", query = "SELECT c FROM CriteriosXPuesto c WHERE c.criteriosXPuestoPK.correlativo = :correlativo"),
     @NamedQuery(name = "CriteriosXPuesto.findByValor", query = "SELECT c FROM CriteriosXPuesto c WHERE c.valor = :valor"),
     @NamedQuery(name = "CriteriosXPuesto.findByValorInicialRango", query = "SELECT c FROM CriteriosXPuesto c WHERE c.valorInicialRango = :valorInicialRango"),
-    @NamedQuery(name = "CriteriosXPuesto.findByValorFinalRango", query = "SELECT c FROM CriteriosXPuesto c WHERE c.valorFinalRango = :valorFinalRango"),
-    @NamedQuery(name = "CriteriosXPuesto.findByCampo", query = "SELECT c FROM CriteriosXPuesto c WHERE c.campo = :campo"),
-    @NamedQuery(name = "CriteriosXPuesto.findByEntidad", query = "SELECT c FROM CriteriosXPuesto c WHERE c.entidad = :entidad")
-    })
-public class CriteriosXPuesto implements Serializable
-{
+    @NamedQuery(name = "CriteriosXPuesto.findByValorFinalRango", query = "SELECT c FROM CriteriosXPuesto c WHERE c.valorFinalRango = :valorFinalRango")})
+public class CriteriosXPuesto implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     @EmbeddedId
     protected CriteriosXPuestoPK criteriosXPuestoPK;
-
     @Size(max = 200)
     @Column(name = "VALOR", length = 200)
     private String valor;
-
     @Size(max = 200)
     @Column(name = "VALOR_INICIAL_RANGO", length = 200)
     private String valorInicialRango;
-
     @Size(max = 200)
     @Column(name = "VALOR_FINAL_RANGO", length = 200)
     private String valorFinalRango;
-
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 200)
-    @Column(name = "CAMPO", nullable = false, length = 200)
-    private String campo;
-
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 200)
-    @Column(name = "ENTIDAD", nullable = false, length = 200)
-    private String entidad;
-
-    @JoinColumns(
-        {
+    @JoinColumns({
         @JoinColumn(name = "COD_CIA", referencedColumnName = "COD_CIA", nullable = false, insertable = false, updatable = false),
         @JoinColumn(name = "PUESTO", referencedColumnName = "COD_PUESTO", nullable = false, insertable = false, updatable = false)
-        })
+    })
     @ManyToOne(optional = false)
     private Puesto puesto1;
-
-    @JoinColumns(
-        {
+    @JoinColumns({
         @JoinColumn(name = "COD_CIA", referencedColumnName = "COD_CIA", nullable = false, insertable = false, updatable = false),
         @JoinColumn(name = "TIPO_CRITERIO", referencedColumnName = "TIPO", nullable = false, insertable = false, updatable = false),
         @JoinColumn(name = "CRITERIO", referencedColumnName = "CODIGO", nullable = false, insertable = false, updatable = false)
-        })
+    })
     @ManyToOne(optional = false)
     private Criterio criterio1;
 
-    public CriteriosXPuesto()
-    {
+    public CriteriosXPuesto() {
     }
 
-    public CriteriosXPuesto(CriteriosXPuestoPK criteriosXPuestoPK)
-    {
+    public CriteriosXPuesto(CriteriosXPuestoPK criteriosXPuestoPK) {
         this.criteriosXPuestoPK = criteriosXPuestoPK;
     }
 
-    public CriteriosXPuesto(CriteriosXPuestoPK criteriosXPuestoPK, String campo, String entidad)
-    {
-        this.criteriosXPuestoPK = criteriosXPuestoPK;
-        this.campo = campo;
-        this.entidad = entidad;
-    }
-
-    public CriteriosXPuesto(long codCia, long puesto, long tipoCriterio, long criterio, long correlativo)
-    {
+    public CriteriosXPuesto(long codCia, long puesto, long tipoCriterio, long criterio, long correlativo) {
         this.criteriosXPuestoPK = new CriteriosXPuestoPK(codCia, puesto, tipoCriterio, criterio, correlativo);
     }
 
-    public CriteriosXPuestoPK getCriteriosXPuestoPK()
-    {
+    public CriteriosXPuestoPK getCriteriosXPuestoPK() {
         return criteriosXPuestoPK;
     }
 
-    public void setCriteriosXPuestoPK(CriteriosXPuestoPK criteriosXPuestoPK)
-    {
+    public void setCriteriosXPuestoPK(CriteriosXPuestoPK criteriosXPuestoPK) {
         this.criteriosXPuestoPK = criteriosXPuestoPK;
     }
 
-    public String getValor()
-    {
+    public String getValor() {
         return valor;
     }
 
-    public void setValor(String valor)
-    {
+    public void setValor(String valor) {
         this.valor = valor;
     }
 
-    public String getValorInicialRango()
-    {
+    public String getValorInicialRango() {
         return valorInicialRango;
     }
 
-    public void setValorInicialRango(String valorInicialRango)
-    {
+    public void setValorInicialRango(String valorInicialRango) {
         this.valorInicialRango = valorInicialRango;
     }
 
-    public String getValorFinalRango()
-    {
+    public String getValorFinalRango() {
         return valorFinalRango;
     }
 
-    public void setValorFinalRango(String valorFinalRango)
-    {
+    public void setValorFinalRango(String valorFinalRango) {
         this.valorFinalRango = valorFinalRango;
     }
 
-    public String getCampo()
-    {
-        return campo;
-    }
-
-    public void setCampo(String campo)
-    {
-        this.campo = campo;
-    }
-
-    public String getEntidad()
-    {
-        return entidad;
-    }
-
-    public void setEntidad(String entidad)
-    {
-        this.entidad = entidad;
-    }
-
-    public Puesto getPuesto1()
-    {
+    public Puesto getPuesto1() {
         return puesto1;
     }
 
-    public void setPuesto1(Puesto puesto1)
-    {
+    public void setPuesto1(Puesto puesto1) {
         this.puesto1 = puesto1;
     }
 
-    public Criterio getCriterio1()
-    {
+    public Criterio getCriterio1() {
         return criterio1;
     }
 
-    public void setCriterio1(Criterio criterio1)
-    {
+    public void setCriterio1(Criterio criterio1) {
         this.criterio1 = criterio1;
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         int hash = 0;
         hash += (criteriosXPuestoPK != null ? criteriosXPuestoPK.hashCode() : 0);
         return hash;
     }
 
     @Override
-    public boolean equals(Object object)
-    {
+    public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof CriteriosXPuesto))
-            {
+        if (!(object instanceof CriteriosXPuesto)) {
             return false;
-            }
+        }
         CriteriosXPuesto other = (CriteriosXPuesto) object;
-        if ((this.criteriosXPuestoPK == null && other.criteriosXPuestoPK != null) || (this.criteriosXPuestoPK != null && !this.criteriosXPuestoPK.equals(other.criteriosXPuestoPK)))
-            {
+        if ((this.criteriosXPuestoPK == null && other.criteriosXPuestoPK != null) || (this.criteriosXPuestoPK != null && !this.criteriosXPuestoPK.equals(other.criteriosXPuestoPK))) {
             return false;
-            }
+        }
         return true;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "com.infosgroup.planilla.modelo.entidades.CriteriosXPuesto[ criteriosXPuestoPK=" + criteriosXPuestoPK + " ]";
     }
-    
 }
